d5eaf78cd4e5035745e796bb9d203164d4eb837b
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2021-07-13 18:37:00 +0200

    Update azumarill.asm

--- data/pokemon/base_stats/azumarill.asm
+++ data/pokemon/base_stats/azumarill.asm
@@ -21,7 +21,7 @@ endc
 	db GROWTH_FAST ; growth rate
 	dn EGG_WATER_1, EGG_FAIRY ; egg groups
 
-	ev_yield   3,   0,   0,   0,   0,   0
+	ev_yield   0,   0,   0,   0,   0,   0
 	;         hp  atk  def  spd  sat  sdf
 
 	; tm/hm learnset
