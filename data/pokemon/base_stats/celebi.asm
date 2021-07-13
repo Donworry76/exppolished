0937ee822fcf8dd24fa70aab3a24e0951b5c45f4
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2021-07-13 18:41:24 +0200

    Update celebi.asm

--- data/pokemon/base_stats/celebi.asm
+++ data/pokemon/base_stats/celebi.asm
@@ -20,7 +20,7 @@ endc
 	db GROWTH_MEDIUM_SLOW ; growth rate
 	dn EGG_NONE, EGG_NONE ; egg groups
 
-	ev_yield   3,   0,   0,   0,   0,   0
+	ev_yield   0,   0,   0,   0,   0,   0
 	;         hp  atk  def  spd  sat  sdf
 
 	; tm/hm learnset
