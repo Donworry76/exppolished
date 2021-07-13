133460b17ffdf8ba02504a0dacb738f6acc04c33
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2021-07-13 18:42:11 +0200

    Update charizard.asm

--- data/pokemon/base_stats/charizard.asm
+++ data/pokemon/base_stats/charizard.asm
@@ -25,7 +25,7 @@ endc
 	db GROWTH_MEDIUM_SLOW ; growth rate
 	dn EGG_MONSTER, EGG_DRAGON ; egg groups
 
-	ev_yield   0,   0,   0,   0,   3,   0
+	ev_yield   0,   0,   0,   0,   0,   0
 	;         hp  atk  def  spd  sat  sdf
 
 	; tm/hm learnset
