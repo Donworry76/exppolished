fe115bae7fb57c7ade5fa5d619d5cb6d118160c1
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2021-07-14 11:45:56 +0200

    Update dodrio.asm

--- data/pokemon/base_stats/dodrio.asm
+++ data/pokemon/base_stats/dodrio.asm
@@ -12,7 +12,7 @@
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_FLYING, EGG_FLYING ; egg groups
 
-	ev_yield   0,   2,   0,   0,   0,   0
+	ev_yield   0,   0,   0,   0,   0,   0
 	;         hp  atk  def  spd  sat  sdf
 
 	; tm/hm learnset
