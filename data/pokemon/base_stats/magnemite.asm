5c70733d4d3b8560df5f62513be60dcd13ab23b6
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2021-07-14 12:11:38 +0200

    Update magnemite.asm

--- data/pokemon/base_stats/magnemite.asm
+++ data/pokemon/base_stats/magnemite.asm
@@ -16,7 +16,7 @@ endc
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_MINERAL, EGG_MINERAL ; egg groups
 
-	ev_yield   0,   0,   0,   0,   1,   0
+	ev_yield   0,   0,   0,   0,   0,   0
 	;         hp  atk  def  spd  sat  sdf
 
 	; tm/hm learnset
