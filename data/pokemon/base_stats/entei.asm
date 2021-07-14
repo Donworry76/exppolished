826beb932ce774ddcf9cf208f30de7661456ed18
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2021-07-14 11:50:08 +0200

    Update entei.asm

--- data/pokemon/base_stats/entei.asm
+++ data/pokemon/base_stats/entei.asm
@@ -16,7 +16,7 @@ endc
 	db GROWTH_SLOW ; growth rate
 	dn EGG_NONE, EGG_NONE ; egg groups
 
-	ev_yield   1,   2,   0,   0,   0,   0
+	ev_yield   0,   0,   0,   0,   0,   0
 	;         hp  atk  def  spd  sat  sdf
 
 	; tm/hm learnset
