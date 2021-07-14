64f5ff1f07375d6ff42f4b260ab4f0993523718f
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2021-07-14 12:12:11 +0200

    Update magnezone.asm

--- data/pokemon/base_stats/magnezone.asm
+++ data/pokemon/base_stats/magnezone.asm
@@ -16,7 +16,7 @@ endc
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_MINERAL, EGG_MINERAL ; egg groups
 
-	ev_yield   0,   0,   0,   0,   3,   0
+	ev_yield   0,   0,   0,   0,   0,   0
 	;         hp  atk  def  spd  sat  sdf
 
 	; tm/hm learnset
