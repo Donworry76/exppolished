4a9ed8327ebbe72df85da14ebf4077b4507685ea
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2021-07-14 12:01:24 +0200

    Update honchkrow.asm

--- data/pokemon/base_stats/honchkrow.asm
+++ data/pokemon/base_stats/honchkrow.asm
@@ -17,7 +17,7 @@ endc
 	db GROWTH_MEDIUM_SLOW ; growth rate
 	dn EGG_FLYING, EGG_FLYING ; egg groups
 
-	ev_yield   0,   2,   0,   0,   0,   0
+	ev_yield   0,   0,   0,   0,   0,   0
 	;         hp  atk  def  spd  sat  sdf
 
 	; tm/hm learnset
