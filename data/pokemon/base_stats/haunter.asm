e1862cfdb23aca301537c0ac18cd747adbba313a
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2021-07-14 11:59:24 +0200

    Update haunter.asm

--- data/pokemon/base_stats/haunter.asm
+++ data/pokemon/base_stats/haunter.asm
@@ -12,7 +12,7 @@
 	db GROWTH_MEDIUM_SLOW ; growth rate
 	dn EGG_INDETERMINATE, EGG_INDETERMINATE ; egg groups
 
-	ev_yield   0,   0,   0,   0,   2,   0
+	ev_yield   0,   0,   0,   0,   0,   0
 	;         hp  atk  def  spd  sat  sdf
 
 	; tm/hm learnset
