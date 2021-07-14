f5c4a486b08c950f1402156b2a8c7cf1b0cfe758
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2021-07-14 12:17:02 +0200

    Update misdreavus.asm

--- data/pokemon/base_stats/misdreavus.asm
+++ data/pokemon/base_stats/misdreavus.asm
@@ -12,7 +12,7 @@
 	db GROWTH_FAST ; growth rate
 	dn EGG_INDETERMINATE, EGG_INDETERMINATE ; egg groups
 
-	ev_yield   0,   0,   0,   0,   0,   1
+	ev_yield   0,   0,   0,   0,   0,   0
 	;         hp  atk  def  spd  sat  sdf
 
 	; tm/hm learnset
