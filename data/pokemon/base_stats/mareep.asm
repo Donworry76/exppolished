9c4490c3d6875483289815c4675142f48bc210f3
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2021-07-14 12:13:14 +0200

    Update mareep.asm

--- data/pokemon/base_stats/mareep.asm
+++ data/pokemon/base_stats/mareep.asm
@@ -16,7 +16,7 @@ endc
 	db GROWTH_MEDIUM_SLOW ; growth rate
 	dn EGG_MONSTER, EGG_GROUND ; egg groups
 
-	ev_yield   0,   0,   0,   0,   1,   0
+	ev_yield   0,   0,   0,   0,   0,   0
 	;         hp  atk  def  spd  sat  sdf
 
 	; tm/hm learnset
