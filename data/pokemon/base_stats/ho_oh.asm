c6a3ccaf451eee698ac3c0b2b828fd7be54df224
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2021-07-14 12:01:07 +0200

    Update ho_oh.asm

--- data/pokemon/base_stats/ho_oh.asm
+++ data/pokemon/base_stats/ho_oh.asm
@@ -16,7 +16,7 @@ endc
 	db GROWTH_SLOW ; growth rate
 	dn EGG_NONE, EGG_NONE ; egg groups
 
-	ev_yield   0,   0,   0,   0,   0,   3
+	ev_yield   0,   0,   0,   0,   0,   0
 	;         hp  atk  def  spd  sat  sdf
 
 	; tm/hm learnset
