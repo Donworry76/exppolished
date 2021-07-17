7207b077136b606af25f34394bda14d48ea4c061
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2021-07-17 12:50:51 +0200

    Update piloswine.asm

--- data/pokemon/base_stats/piloswine.asm
+++ data/pokemon/base_stats/piloswine.asm
@@ -12,7 +12,7 @@
 	db GROWTH_SLOW ; growth rate
 	dn EGG_GROUND, EGG_GROUND ; egg groups
 
-	ev_yield   1,   1,   0,   0,   0,   0
+	ev_yield   0,   0,   0,   0,   0,   0
 	;         hp  atk  def  spd  sat  sdf
 
 	; tm/hm learnset
