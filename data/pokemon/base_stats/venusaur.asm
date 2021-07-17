cc370c22e8df242cee0066604b2c547d398a87e3
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2021-07-17 15:15:22 +0200

    Update venusaur.asm

--- data/pokemon/base_stats/venusaur.asm
+++ data/pokemon/base_stats/venusaur.asm
@@ -12,7 +12,7 @@
 	db GROWTH_MEDIUM_SLOW ; growth rate
 	dn EGG_MONSTER, EGG_PLANT ; egg groups
 
-	ev_yield   0,   0,   0,   0,   2,   1
+	ev_yield   0,   0,   0,   0,   0,   0
 	;         hp  atk  def  spd  sat  sdf
 
 	; tm/hm learnset
