2c6632a7779cbeb57e3f30c88f4779ee761a9b3f
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2021-07-17 12:47:21 +0200

    Update pidgeot.asm

--- data/pokemon/base_stats/pidgeot.asm
+++ data/pokemon/base_stats/pidgeot.asm
@@ -21,7 +21,7 @@ endc
 	db GROWTH_MEDIUM_SLOW ; growth rate
 	dn EGG_FLYING, EGG_FLYING ; egg groups
 
-	ev_yield   0,   0,   0,   3,   0,   0
+	ev_yield   0,   0,   0,   0,   0,   0
 	;         hp  atk  def  spd  sat  sdf
 
 	; tm/hm learnset
