ba59f1d770df60127909712300eb7fd8fd58e8c3
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2021-07-17 15:15:02 +0200

    Update victreebel.asm

--- data/pokemon/base_stats/victreebel.asm
+++ data/pokemon/base_stats/victreebel.asm
@@ -21,7 +21,7 @@ endc
 	db GROWTH_MEDIUM_SLOW ; growth rate
 	dn EGG_PLANT, EGG_PLANT ; egg groups
 
-	ev_yield   0,   3,   0,   0,   0,   0
+	ev_yield   0,   0,   0,   0,   0,   0
 	;         hp  atk  def  spd  sat  sdf
 
 	; tm/hm learnset
