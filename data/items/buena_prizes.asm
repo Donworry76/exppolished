11f178c2528eabe38017a4c9c517ce811e1fcf9a
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2021-07-19 16:47:32 +0200

    Update buena_prizes.asm

--- data/items/buena_prizes.asm
+++ data/items/buena_prizes.asm
@@ -5,7 +5,7 @@ BlueCardMartData:
 	db CUSTAP_BERRY, 1
 	db BOTTLE_CAP,   1
 	db ENIGMA_BERRY, 1
-	db RARE_CANDY,   1
+	db ABILITYPATCH, 1
 	db EVIOLITE,     1
 	db PP_MAX,       1
 	db ABILITY_CAP,  1
