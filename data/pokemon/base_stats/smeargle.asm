d7a4eda31fc403b6d2d411372f4798ef26c6b523
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2021-07-19 13:54:50 +0200

    Update smeargle.asm

--- data/pokemon/base_stats/smeargle.asm
+++ data/pokemon/base_stats/smeargle.asm
@@ -2,7 +2,7 @@ if DEF(FAITHFUL)
 	db  55,  20,  35,  75,  20,  45 ; 250 BST
 	;   hp  atk  def  spd  sat  sdf
 else
-	db  55,  20,  35, 100,  20,  45 ; 275 BST
+	db  75,  40,  35, 120,  40,  45 ; 275 BST
 	;   hp  atk  def  spd  sat  sdf
 endc
 
