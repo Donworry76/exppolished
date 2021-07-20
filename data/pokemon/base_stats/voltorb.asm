4bfc26133e8337230def70f2e0bf55ef33d4d1d2
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2021-07-20 09:52:56 +0200

    Update voltorb.asm

--- data/pokemon/base_stats/voltorb.asm
+++ data/pokemon/base_stats/voltorb.asm
@@ -8,7 +8,7 @@
 	db CELL_BATTERY ; item 2
 	dn GENDER_UNKNOWN, 3 ; gender ratio, step cycles to hatch
 	INCBIN "gfx/pokemon/voltorb/front.dimensions"
-	abilities_for VOLTORB, SOUNDPROOF, STATIC, AFTERMATH
+	abilities_for VOLTORB, SOUNDPROOF, GALVANIZE, AFTERMATH
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_MINERAL, EGG_MINERAL ; egg groups
 
