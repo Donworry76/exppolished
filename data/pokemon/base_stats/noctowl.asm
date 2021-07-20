76c5d2319e94cb5611cf450f9b05d39a8eda6032
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2021-07-20 10:00:18 +0200

    Update noctowl.asm

--- data/pokemon/base_stats/noctowl.asm
+++ data/pokemon/base_stats/noctowl.asm
@@ -2,7 +2,7 @@ if DEF(FAITHFUL)
 	db 100,  50,  50,  70,  86,  96 ; 452 BST
 	;   hp  atk  def  spd  sat  sdf
 else
-	db 100,  50,  50,  70,  96,  96 ; 462 BST
+	db 110,  50,  50,  70,  96,  96 ; 462 BST
 	;   hp  atk  def  spd  sat  sdf
 endc
 
