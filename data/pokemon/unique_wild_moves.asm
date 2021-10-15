9430bf9a1c44eb395b624fb268b5250fe0499394
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2021-10-15 20:31:55 +0200

    Update unique_wild_moves.asm

--- data/pokemon/unique_wild_moves.asm
+++ data/pokemon/unique_wild_moves.asm
@@ -7,8 +7,8 @@ UniqueWildMoves:
 	db UNION_CAVE,       LAPRAS,     SHELL_SMASH  ; UnionCaveB2F, Fridays
 	db CHERRYGROVE_BAY,  CORSOLA,    SUCKER_PUNCH ; HGSS tutor move
 	db SPROUT_TOWER,     BELLSPROUT, GROWTH       ; knows Growth early
-	db ROUTE_32_COAST,   FLAAFFY,    PURSUIT      ; Prism tutor move
-	db RUINS_OF_ALPH,    SHUCKLE,    ANCIENTPOWER ; Sw/Sh move
+	db ROUTE_32_COAST,   FLAAFFY,    EARTH_POWER  ; Prism tutor move
+	db RUINS_OF_ALPH,    SHUCKLE,    SEISMIC_TOSS ; Sw/Sh move
 	db RUINS_OF_ALPH,    SMEARGLE,   SKETCH       ; knows Sketch twice
 	db SLOWPOKE_WELL,    SLOWPOKE,   GROWTH       ; Prism tutor move
 	db ILEX_FOREST,      0,          0
@@ -32,44 +32,44 @@ UniqueWildMoves:
 	db DRAGONS_DEN,      0,          0
 	db DARK_CAVE,        0,          0
 	db SILVER_CAVE,      TYRANITAR,  DRAGON_DANCE ; event move
-	db VIRIDIAN_FOREST,  PIKACHU,    DISARM_VOICE ; Sketch-breed move
+	db VIRIDIAN_FOREST,  PIKACHU,    MOONBLAST    ; Sketch-breed move
 	db MT_MOON,          0,          0
-	db CERULEAN_CAVE,    MACHAMP,    METRONOME    ; RBY TM move
-	db CERULEAN_CAVE,    GOLEM,      METRONOME    ; RBY TM move
-	db CERULEAN_CAVE,    GENGAR,     METRONOME    ; RBY TM move
-	db CERULEAN_CAVE,    ALAKAZAM,   METRONOME    ; RBY TM move
-	db CERULEAN_CAPE,    POLIWHIRL,  METRONOME    ; RBY TM move
-	db DIGLETTS_CAVE,    DUGTRIO,    SCREECH      ; Sw/Sh move
-	db ROCK_TUNNEL,      ELECTABUZZ, METRONOME    ; RBY TM move
+	db CERULEAN_CAVE,    MACHAMP,    DIZZY_PUNCH  ; RBY TM move
+	db CERULEAN_CAVE,    GOLEM,      SHELL_SMASH  ; RBY TM move
+	db CERULEAN_CAVE,    GENGAR,     AURA_SPHERE  ; RBY TM move
+	db CERULEAN_CAVE,    ALAKAZAM,   ANCIENTPOWER ; RBY TM move
+	db CERULEAN_CAPE,    POLIWHIRL,  ZEN_HEADBUTT ; RBY TM move
+	db DIGLETTS_CAVE,    DUGTRIO,    MEGAHORN     ; Sw/Sh move
+	db ROCK_TUNNEL,      ELECTABUZZ, CRABHAMMER   ; RBY TM move
 	db DIM_CAVE,         GRAVELER,   SUCKER_PUNCH ; HGSS tutor move
 	db DIM_CAVE,         RHYDON,     CRUNCH       ; Prism tutor move
-	db SOUL_HOUSE,       HAUNTER,    MOONBLAST    ; Prism tutor move
+	db SOUL_HOUSE,       HAUNTER,    AURA_SPHERE  ; Prism tutor move
 	db LAV_RADIO_TOWER,  MAROWAK,    PERISH_SONG  ; Dream World move
-	db LUCKY_ISLAND,     CHANSEY,    TRI_ATTACK   ; RBY TM move
+	db LUCKY_ISLAND,     CHANSEY,    SUPER_FANG   ; RBY TM move
 	db LUCKY_ISLAND,     TOGETIC,    0
 	db SAFARI_ZONE,      TANGELA,    LEECH_SEED   ; Dream World move (hub)
-	db SAFARI_ZONE,      TAUROS,     STOMP        ; RBY move (east)
-	db SAFARI_ZONE,      PINSIR,     SLASH        ; RBY move (north)
+	db SAFARI_ZONE,      TAUROS,     EXTREMESPEED ; RBY move (east)
+	db SAFARI_ZONE,      PINSIR,     SHELL_SMASH  ; RBY move (north)
 	db SAFARI_ZONE,      KANGASKHAN, 0            ; (west)
 	db URAGA_CHANNEL,    TENTACRUEL, RECOVER      ; TCG move
-	db SCARY_CAVE,       GOLBAT,     HYPNOSIS     ; Dream World move
+	db SCARY_CAVE,       GOLBAT,     AEROBLAST    ; Dream World move
 	db SCARY_CAVE,       LANTURN,    SUCKER_PUNCH ; HGSS tutor move
-	db SEAFOAM_ISLANDS,  JYNX,       METRONOME    ; RBY TM move
+	db SEAFOAM_ISLANDS,  JYNX,       PAIN_SPLIT   ; RBY TM move
 	db POKEMON_MANSION,  RAPIDASH,   NASTY_PLOT   ; Prism tutor move
-	db CINNABAR_VOLCANO, MAGMAR,     METRONOME    ; RBY TM move
+	db CINNABAR_VOLCANO, MAGMAR,     AURA_SPHERE  ; RBY TM move
 	db TOHJO_FALLS,      0,          0
 	db VICTORY_ROAD,     0,          0
 	db SHAMOUTI_ISLAND,  0,          0
 	db BEAUTIFUL_BEACH,  0,          0
-	db ROCKY_BEACH,      DONPHAN,    LOW_KICK     ; HGSS tutor move
+	db ROCKY_BEACH,      DONPHAN,    WILD_CHARGE  ; HGSS tutor move
 	db NOISY_FOREST,     SCIZOR,     CRABHAMMER   ; new move
 	db NOISY_FOREST,     PINECO,     PAIN_SPLIT   ; HGSS tutor move
-	db SHRINE_RUINS,     HYPNO,      METRONOME    ; RBY TM move
+	db SHRINE_RUINS,     HYPNO,      PSYSTRIKE    ; RBY TM move
 	db SHAMOUTI_TUNNEL,  GOLBAT,     OUTRAGE      ; Prism tutor move
 	db WARM_BEACH,       SEADRA,     FLAMETHROWER ; new move
 	db SHAMOUTI_COAST,   0,          0
 	db FIRE_ISLAND,      HOUNDOOM,   SUCKER_PUNCH ; HGSS tutor move
-	db ICE_ISLAND,       WEAVILE,    LOW_KICK     ; HGSS tutor move
+	db ICE_ISLAND,       WEAVILE,    DRAIN_PUNCH  ; HGSS tutor move
 	db LIGHTNING_ISLAND, ELECTRODE,  SUCKER_PUNCH ; HGSS tutor move
 	db ROUTE_49,         PARASECT,   CONFUSE_RAY  ; Prism tutor move
 	db -1
