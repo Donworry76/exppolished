5aead435c92e429bb5e4899b25e62fb81c89e538
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2023-02-01 15:03:59 +0100

    Update lanturn.asm

--- data/pokemon/base_stats/lanturn.asm
+++ data/pokemon/base_stats/lanturn.asm
@@ -2,7 +2,7 @@ if DEF(FAITHFUL)
 	db 125,  58,  58,  67,  76,  76 ; 460 BST
 	;   hp  atk  def  spd  sat  sdf
 else
-	db 125,  58,  58,  67,  86,  86 ; 480 BST
+	db 125,  58,  88,  67,  96,  96 ; 480 BST
 	;   hp  atk  def  spd  sat  sdf
 endc
 
