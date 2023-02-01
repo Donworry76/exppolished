e53b73fb0dd21743933201147e7bf56e89be274d
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2023-02-01 14:53:30 +0100

    Update steelix.asm

--- data/pokemon/base_stats/steelix.asm
+++ data/pokemon/base_stats/steelix.asm
@@ -2,7 +2,7 @@ if DEF(FAITHFUL)
 	db  75,  85, 200,  30,  55,  65 ; 510 BST
 	;   hp  atk  def  spd  sat  sdf
 else
-	db  75,  90, 200,  30,  55,  65 ; 515 BST
+	db  75, 110, 200,  30,  55,  65 ; 515 BST
 	;   hp  atk  def  spd  sat  sdf
 endc
 
