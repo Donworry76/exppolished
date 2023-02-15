5c4260d7136fec8848994a9ecbc8c25b6b21a0d6
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2023-02-15 15:40:40 +0100

    Update qwilfish.asm

--- data/pokemon/base_stats/qwilfish.asm
+++ data/pokemon/base_stats/qwilfish.asm
@@ -17,7 +17,7 @@ endc
 	db POISON_BARB ; item 2
 	dn GENDER_F50, 3 ; gender ratio, step cycles to hatch
 	INCBIN "gfx/pokemon/qwilfish/front.dimensions"
-	abilities_for QWILFISH, POISON_POINT, SWIFT_SWIM, INTIMIDATE
+	abilities_for QWILFISH, MULTI_SCALE, MULTI_SCALE, INTIMIDATE
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_WATER_2, EGG_WATER_2 ; egg groups
 
