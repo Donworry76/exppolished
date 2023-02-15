8f4a2e881a54dd8e7b1f0d1508a701e67b4ec6ab
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2023-02-15 15:25:02 +0100

    Update exeggutor_plain.asm

--- data/pokemon/base_stats/exeggutor_plain.asm
+++ data/pokemon/base_stats/exeggutor_plain.asm
@@ -8,7 +8,7 @@
 	db MARANGABERRY ; item 2
 	dn GENDER_F50, 3 ; gender ratio, step cycles to hatch
 	INCBIN "gfx/pokemon/exeggutor_plain/front.dimensions"
-	abilities_for EXEGGUTOR, CHLOROPHYLL, CHLOROPHYLL, HARVEST
+	abilities_for EXEGGUTOR, FILTER, FILTER, CHLOROPHYLL
 	db GROWTH_SLOW ; growth rate
 	dn EGG_PLANT, EGG_PLANT ; egg groups
 
