32ff01f870d61ef4c82bb5671f3091ae3a15ffde
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2023-02-15 15:40:55 +0100

    Update pinsir.asm

--- data/pokemon/base_stats/pinsir.asm
+++ data/pokemon/base_stats/pinsir.asm
@@ -8,7 +8,7 @@
 	db NO_ITEM ; item 2
 	dn GENDER_F50, 4 ; gender ratio, step cycles to hatch
 	INCBIN "gfx/pokemon/pinsir/front.dimensions"
-	abilities_for PINSIR, HYPER_CUTTER, MOLD_BREAKER, MOXIE
+	abilities_for PINSIR, INTIMIDATE, MOLD_BREAKER, MOXIE
 	db GROWTH_SLOW ; growth rate
 	dn EGG_BUG, EGG_BUG ; egg groups
 
