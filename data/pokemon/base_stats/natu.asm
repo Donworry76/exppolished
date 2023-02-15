a6499705c47c0e3a8b8df334b39817a123ef966f
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2023-02-15 15:36:33 +0100

    Update natu.asm

--- data/pokemon/base_stats/natu.asm
+++ data/pokemon/base_stats/natu.asm
@@ -8,7 +8,7 @@
 	db NO_ITEM ; item 2
 	dn GENDER_F50, 3 ; gender ratio, step cycles to hatch
 	INCBIN "gfx/pokemon/natu/front.dimensions"
-	abilities_for NATU, SYNCHRONIZE, EARLY_BIRD, MAGIC_BOUNCE
+	abilities_for NATU, SYNCHRONIZE, MAGIC_GUARD, MAGIC_BOUNCE
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_FLYING, EGG_FLYING ; egg groups
 
