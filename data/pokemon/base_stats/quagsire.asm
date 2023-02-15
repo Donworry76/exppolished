11a9101d61a0e2f99c349c029fa086519acf4bc4
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2023-02-15 15:37:55 +0100

    Update quagsire.asm

--- data/pokemon/base_stats/quagsire.asm
+++ data/pokemon/base_stats/quagsire.asm
@@ -13,7 +13,7 @@ endc
 	db CHESTO_BERRY ; item 2
 	dn GENDER_F50, 3 ; gender ratio, step cycles to hatch
 	INCBIN "gfx/pokemon/quagsire/front.dimensions"
-	abilities_for QUAGSIRE, DAMP, WATER_ABSORB, UNAWARE
+	abilities_for QUAGSIRE, SIMPLE, WATER_ABSORB, UNAWARE
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_WATER_1, EGG_GROUND ; egg groups
 
