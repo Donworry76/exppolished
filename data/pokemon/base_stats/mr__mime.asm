91ab236a91b93d2702b86eb9310a6af3e72002a6
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2023-02-15 15:28:19 +0100

    Update mr__mime.asm

--- data/pokemon/base_stats/mr__mime.asm
+++ data/pokemon/base_stats/mr__mime.asm
@@ -13,7 +13,7 @@ endc
 	db PERSIM_BERRY ; item 2
 	dn GENDER_F50, 4 ; gender ratio, step cycles to hatch
 	INCBIN "gfx/pokemon/mr__mime/front.dimensions"
-	abilities_for MR__MIME, SOUNDPROOF, FILTER, TECHNICIAN
+	abilities_for MR__MIME, FILTER, FILTER, TECHNICIAN
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_HUMANSHAPE, EGG_HUMANSHAPE ; egg groups
 
