14e7aa201c2ce5ff216ab3617c033695e7d021f8
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2023-02-15 14:54:06 +0100

    Update ivysaur.asm

--- data/pokemon/base_stats/ivysaur.asm
+++ data/pokemon/base_stats/ivysaur.asm
@@ -8,7 +8,7 @@
 	db NO_ITEM ; item 2
 	dn GENDER_F12_5, 3 ; gender ratio, step cycles to hatch
 	INCBIN "gfx/pokemon/ivysaur/front.dimensions"
-	abilities_for IVYSAUR, OVERGROW, CHLOROPHYLL, EFFECT_SPORE
+	abilities_for IVYSAUR, EFFECT_SPORE, CHLOROPHYLL, EFFECT_SPORE
 	db GROWTH_MEDIUM_SLOW ; growth rate
 	dn EGG_MONSTER, EGG_PLANT ; egg groups
 
