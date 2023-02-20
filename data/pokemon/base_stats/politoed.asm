0dda0d23bdd0a7b789b94e5d20207584ac74204f
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2023-02-20 10:21:51 +0100

    Update politoed.asm

--- data/pokemon/base_stats/politoed.asm
+++ data/pokemon/base_stats/politoed.asm
@@ -17,7 +17,7 @@ endc
 	db KINGS_ROCK ; item 2
 	dn GENDER_F50, 3 ; gender ratio, step cycles to hatch
 	INCBIN "gfx/pokemon/politoed/front.dimensions"
-	abilities_for POLITOED, WATER_ABSORB, DAMP, DRIZZLE
+	abilities_for POLITOED, DRIZZLE, DRIZZLE, DRIZZLE
 	db GROWTH_MEDIUM_SLOW ; growth rate
 	dn EGG_WATER_1, EGG_WATER_1 ; egg groups
 
