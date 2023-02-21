bf78b6ad96e0ee3ddd80b9c4b105fe3ac1be75bc
Donworry76 <69807942+Donworry76@users.noreply.github.com>
2023-02-21 11:28:59 +0100

    Update slowbro_plain.asm

--- data/pokemon/base_stats/slowbro_plain.asm
+++ data/pokemon/base_stats/slowbro_plain.asm
@@ -13,7 +13,7 @@ endc
 	db KINGS_ROCK ; item 2
 	dn GENDER_F50, 3 ; gender ratio, step cycles to hatch
 	INCBIN "gfx/pokemon/slowbro_plain/front.dimensions"
-	abilities_for SLOWBRO, OBLIVIOUS, OWN_TEMPO, REGENERATOR
+	abilities_for SLOWBRO, QUICK_DRAW, QUICK_DRAW, REGENERATOR
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_MONSTER, EGG_WATER_1 ; egg groups
 
